8ed8e6117b8d109fbcce696dd2767af457d3aa6c
anhthaodinh98 <anhthaodinh98@gmail.com>
2023-08-23 22:32:55 +0700

    Allure report

--- src/test/java/net/atlassian/api/issues/TC002.java
+++ src/test/java/net/atlassian/api/issues/TC002.java
@@ -6,28 +6,24 @@ import io.qameta.allure.Description;
 import io.restassured.response.Response;
 import net.atlassian.api.Issues;
 import net.atlassian.api.TestBase;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import ultilities.DataGenerator;
 
 import java.io.IOException;
 
-public class TC001 extends TestBase {
+public class TC002 extends TestBase {
     SoftAssert softAssert = new SoftAssert();
 
-    @Test(description = "This is test case 1")
-    @Description("This is test case 1 allure")
+    @Test(description = "This is test case 2")
+    @Description("This is test case 2 allure")
     public void TC001 () throws IOException {
         Allure.step("Step 1: run post request");
         Response response = Issues.createIssue(JiraCloud.PROJECT_ID, JiraCloud.ISSUE_TYPE_STORY, DataGenerator.randomDescription(), JiraCloud.USER_ID);
         int statusCode = response.statusCode();
-        Allure.step("Step 2: Assert status is 201");
-        softAssert.assertEquals(statusCode, 201);
+        Allure.step("Step 2: Assert status is 200");
+        softAssert.assertEquals(statusCode, 200);
         Object o = response.path("key");
-
-        Allure.step("Step 3: Assert jira id");
-        softAssert.assertTrue(o.toString().matches("PAP-\\d+"));
         softAssert.assertAll();
     }
 }
